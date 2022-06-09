9f104a107d140b6a17fa767b0ca754b9980ed948
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2022-06-09 11:06:38 +0900

    page transion

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Page 1</h1>
+    <nuxt-link to="./page2">page2</nuxt-link>
+  </div>
+</template>
+<script setup>
+definePageMeta({
+  pageTransition: { name: "page", mode: "out-in" },
+});
+</script>
---
+++ pages/page2.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Page 2</h1>
+     <nuxt-link to="./">page1</nuxt-link>
+  </div>
+</template>
+<script setup>
+definePageMeta({
+  pageTransition: { name: "page", mode: "out-in" },
+});
+</script>
